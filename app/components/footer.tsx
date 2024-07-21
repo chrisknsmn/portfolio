8e72ee3ccd7d94f0dd5e150b943326df2d4a84fb
Chris Kinsman <chrisknsmn@gmail.com>
2024-07-21 14:41:31 -0400

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,11 +3,13 @@ export default function Footer() {
   return (
     <footer className="mt-2 p-6 rounded-xl bg-foreground text-background text-center">
       <div className="flex">
-        <div className="flex items-center w-full ">
-          <div>&copy; Chris Kinsman {new Date().getFullYear()}</div>
+        <div className="flex items-center w-full">
+          <div className="text-md sm:text-xs">
+            &copy; Chris Kinsman {new Date().getFullYear()}
+          </div>
         </div>
         <div className="w-full items-center flex justify-end">
-          <Logo className="text-background fill-background max-h-[40px]" />
+          <Logo className="text-background fill-background h-[40px]" />
         </div>
       </div>
     </footer>
