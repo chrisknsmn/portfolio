621e46f6c3c919d0e1713e6b391233f70c7f03e2
Chris Kinsman <chrisknsmn@gmail.com>
2024-07-24 18:42:44 -0400

    Update banner.tsx

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -8,9 +8,7 @@ export default function Banner() {
   return (
     <div className="py-24 flex w-full justify-between items-center">
       <div className="w-full pl-6">
-        <h1 className="md:text-8xl text-6xl font-bold mb-8">
-          Chris <span className="whitespace-nowrap">Kinsman 🚀</span>
-        </h1>
+        <h1 className="md:text-8xl text-6xl font-bold mb-8">Chris Kinsman</h1>
         <h2 className="text-2xl font-regular mb-4 mt-2 max-w-[500px]">
           Full Stack Engineer and Entrepreneur. Passionate about building
           innovative solutions and helping businesses grow.
