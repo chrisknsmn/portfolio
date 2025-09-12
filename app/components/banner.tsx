57c6186bc0940a9e526b4627f70ac5daf9faf60d
chrisknsmn <116185710+chrisknsmn@users.noreply.github.com>
2025-09-12 14:04:54 -0400

    bnr

---
+++ app/components/banner.tsx
@@ -0,0 +1,11 @@
+export default function Banner() {
+  return (
+    <div>
+      <h1>Chris Kinsman</h1>
+      <h2>Development/Design</h2>
+      <p>
+        Front-End & Full-Stack Web Developer | 6+ YOE Delivering Scalable, User-Centered Apps for Enterprise & Modern Web (RBC, Fidelity) | React, Next.js, TypeScript | Reliable, Detail-Driven, Outcome-Focused
+      </p>
+    </div>
+  );
+}
