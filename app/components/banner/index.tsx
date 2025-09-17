053e47510c66acc999cfa3bccb1219d2187f399a
chrisknsmn <116185710+chrisknsmn@users.noreply.github.com>
2025-09-17 13:25:14 -0400

    wavy banner

--- app/components/banner/index.tsx
+++ app/components/banner/index.tsx
@@ -1,14 +1,30 @@
+import { WavyBackground } from "./wavy-background";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Banner() {
   return (
-    <div>
-      <h1 className="font-bold leading-tight text-[clamp(1.75rem,calc(1rem+6vw),5rem)]">
-        Chris Kinsman
-      </h1>
-      <p className="text-sm opacity-90">
-        Front-End & Full-Stack Web Developer | 6+ YOE Delivering Scalable,
-        User-Centered Apps for Enterprise & Modern Web (RBC, Fidelity) | React,
-        Next.js, TypeScript | Reliable, Detail-Driven, Outcome-Focused
-      </p>
+    <div className="border rounded-md overflow-hidden relative">
+      <WavyBackground
+        className=""
+        containerClassName="h-full"
+        waveOpacity={0.3}
+      >
+        <div className="mt-24 p-4 pt-2 sm:pt-0">
+          <h1 className="leading-tight text-[clamp(1.75rem,calc(1rem+6vw),5rem)]">
+            Chris Kinsman
+          </h1>
+          <p className="text-sm opacity-90 mb-4">
+            Front-End & Full-Stack Web Developer | 6+ YOE Delivering Scalable,
+            User-Centered Apps for Enterprise & Modern Web (RBC, Fidelity) |
+            React, Next.js, TypeScript | Reliable, Detail-Driven,
+            Outcome-Focused
+          </p>
+          <Link href="/">
+            <Button>Contact</Button>
+          </Link>
+        </div>
+      </WavyBackground>
     </div>
   );
 }
---
+++ app/components/banner/wavy-background.tsx
@@ -0,0 +1,137 @@
+"use client";
+import { cn } from "@/lib/utils";
+import React, { useEffect, useRef, useState } from "react";
+import { createNoise3D } from "simplex-noise";
+
+export const WavyBackground = ({
+  children,
+  className,
+  containerClassName,
+  colors,
+  waveWidth,
+  backgroundFill,
+  blur = 10,
+  speed = "fast",
+  waveOpacity = 0.5,
+  ...props
+}: {
+  children?: any;
+  className?: string;
+  containerClassName?: string;
+  colors?: string[];
+  waveWidth?: number;
+  backgroundFill?: string;
+  blur?: number;
+  speed?: "slow" | "fast";
+  waveOpacity?: number;
+  [key: string]: any;
+}) => {
+  const noise = createNoise3D();
+  let w: number,
+    h: number,
+    nt: number,
+    i: number,
+    x: number,
+    ctx: any,
+    canvas: any;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const getSpeed = () => {
+    switch (speed) {
+      case "slow":
+        return 0.001;
+      case "fast":
+        return 0.002;
+      default:
+        return 0.001;
+    }
+  };
+
+  const init = () => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext("2d");
+    w = ctx.canvas.width = canvas.parentElement.offsetWidth;
+    h = ctx.canvas.height = 100;
+    ctx.filter = `blur(${blur}px)`;
+    nt = 0;
+    window.onresize = function () {
+      w = ctx.canvas.width = canvas.parentElement.offsetWidth;
+      h = ctx.canvas.height = 100;
+      ctx.filter = `blur(${blur}px)`;
+    };
+    render();
+  };
+
+  // const waveColors = colors ?? [
+  //   "#38bdf8",
+  //   "#818cf8",
+  //   "#c084fc",
+  //   "#e879f9",
+  //   "#22d3ee",
+  // ];
+  const waveColors = colors ?? [
+    "#10b981",
+    "#34d399",
+    "#22d3ee",
+    "#86efac",
+    "#bbf7d0",
+  ];
+  const drawWave = (n: number) => {
+    nt += getSpeed();
+    for (i = 0; i < n; i++) {
+      ctx.beginPath();
+      ctx.lineWidth = waveWidth || 50;
+      ctx.strokeStyle = waveColors[i % waveColors.length];
+      for (x = 0; x < w; x += 5) {
+        var y = noise(x / 800, 0.3 * i, nt) * 50;
+        ctx.lineTo(x, y + 100); // center waves at 100px (middle of 300px)
+      }
+      ctx.stroke();
+      ctx.closePath();
+    }
+  };
+
+  let animationId: number;
+  const render = () => {
+    ctx.clearRect(0, 0, w, h);
+    ctx.globalAlpha = waveOpacity || 0.5;
+    drawWave(5);
+    animationId = requestAnimationFrame(render);
+  };
+
+  useEffect(() => {
+    init();
+    return () => {
+      cancelAnimationFrame(animationId);
+    };
+  }, []);
+
+  const [isSafari, setIsSafari] = useState(false);
+  useEffect(() => {
+    setIsSafari(
+      typeof window !== "undefined" &&
+        navigator.userAgent.includes("Safari") &&
+        !navigator.userAgent.includes("Chrome")
+    );
+  }, []);
+
+  return (
+    <div
+      className={cn(
+        "flex flex-col items-start justify-start relative",
+        containerClassName
+      )}
+    >
+      <canvas
+        className="absolute top-0 left-0 w-full h-24 z-0"
+        ref={canvasRef}
+        id="canvas"
+        style={{
+          ...(isSafari ? { filter: `blur(${blur}px)` } : {}),
+        }}
+      ></canvas>
+      <div className={cn("relative z-10", className)} {...props}>
+        {children}
+      </div>
+    </div>
+  );
+};
