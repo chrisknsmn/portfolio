3f6592358ea6fc4567ee9dd638ff0c81774923e3
chrisknsmn <116185710+chrisknsmn@users.noreply.github.com>
2025-09-29 11:02:45 -0400

    Update index.jsx

--- app/components/contact/index.jsx
+++ app/components/contact/index.jsx
@@ -12,7 +12,9 @@ export default function Contact() {
 
   return (
     <div className="p-4">
-      <H2 id="contact">Contact</H2>
+      <H2 id="contact" className="mb-2">
+        Contact
+      </H2>
       <div>
         <div className="border flex flex-col">
           <div className="border-b px-2 font-bold">Resources</div>
