fe638e00822b34ee623f785634edc7681dd9b59d
chrisknsmn <116185710+chrisknsmn@users.noreply.github.com>
2026-03-08 11:29:35 -0400

    Update index.jsx

--- app/components/footer/index.jsx
+++ app/components/footer/index.jsx
@@ -2,7 +2,7 @@ import { H2, H3, H4, P } from "@/components/ui/main";
 export default function Footer() {
   return (
     <div className="mx-8 py-2 border-t text-center">
-      <P className="p-0">© {new Date().getFullYear()} Chris Kisnman</P>
+      <P className="p-0">© {new Date().getFullYear()} Chris Kinsman</P>
     </div>
   );
 }
